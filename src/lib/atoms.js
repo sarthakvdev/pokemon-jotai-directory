52e1604fcaa8a9d706301c65dd0db1a95c97b56c
Sarthak Verma <sarthakvdev@gmail.com>
2022-06-04 01:42:12 +0530

    notes and jsx

--- src/lib/atoms.js
+++ src/lib/atoms.js
@@ -1,5 +1,13 @@
 import { atom } from "jotai";
 
+/* 
+  Note:
+  Created Atoms ie. pokemonAtom, filterAtom and filteredPokemonAtom
+  All these are globally accessible from a hook `useAtom`
+  which is like `useState` but global
+  Atom uses a bottom-up approach.
+*/
+
 const URL =
   "https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json";
 
